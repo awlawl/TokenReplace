ae46c19e9b4093e532b5ff87e99a9701edf0cfba
Allen Lyons <allen.lyons@gmail.com>
2012-06-27 18:20:25 -0400

    Basicly working

---
+++ TokenReplace/Program.cs
@@ -0,0 +1,29 @@
+﻿
+using System.Configuration;
+using System.IO;
+
+namespace TokenReplace
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //read tokenized file
+            var tokenizedFile = File.ReadAllText(args[0]);
+
+            //enumerate over the appSettings
+            foreach (var settingName in ConfigurationManager.AppSettings.AllKeys)
+            {
+                var setting = ConfigurationManager.AppSettings[settingName];
+
+                //replace all instances of the token with the setting
+                tokenizedFile = tokenizedFile.Replace("@" + settingName + "@", setting);
+            }
+
+            
+            //write the output to the file
+            File.WriteAllText(args[1],tokenizedFile);
+
+        }
+    }
+}
