d42b0ba3e801b3a38694ade984e432ea8bffc25c
Allen Lyons <allen.lyons@gmail.com>
2012-06-27 22:40:22 -0400

    Added some validation

--- TokenReplace/Program.cs
+++ TokenReplace/Program.cs
@@ -1,29 +1,50 @@
 ﻿
+using System;
 using System.Configuration;
 using System.IO;
+using System.Security;
 
 namespace TokenReplace
 {
     class Program
     {
-        static void Main(string[] args)
+        static int Main(string[] args)
         {
+            if (args.Length != 2)
+            {
+                Console.WriteLine("usage: TokenReplace <tokenized file> <output file>");
+                Console.WriteLine("");
+                return -1;
+            }
+
             //read tokenized file
             var tokenizedFile = File.ReadAllText(args[0]);
 
-            //enumerate over the appSettings
-            foreach (var settingName in ConfigurationManager.AppSettings.AllKeys)
+            if (ConfigurationManager.AppSettings.AllKeys.Length == 0)
             {
-                var setting = ConfigurationManager.AppSettings[settingName];
+                Console.WriteLine("FYI, there were no settings in the TokenReplace.exe.config.");
+                return -1;
+            }
+            else
+            {
+                //enumerate over the appSettings
+                foreach (var settingName in ConfigurationManager.AppSettings.AllKeys)
+                {
+                    var setting = ConfigurationManager.AppSettings[settingName];
 
-                //replace all instances of the token with the setting
-                tokenizedFile = tokenizedFile.Replace("@" + settingName + "@", setting);
+                    //replace all instances of the token with the setting
+                    tokenizedFile = tokenizedFile.Replace("@" + settingName + "@", setting);
+                    tokenizedFile = tokenizedFile.Replace("%" + settingName + "%", SecurityElement.Escape(setting));
+                }
+
+
+                //write the output to the file
+                File.WriteAllText(args[1], tokenizedFile);
+
+
             }
 
-            
-            //write the output to the file
-            File.WriteAllText(args[1],tokenizedFile);
-
+            return 0;
         }
     }
 }
